7465356c2f739a386dd92aa72713dc75052e4f24
Muhammad Fauzan Laptop <muhammadfauzan7@gmail.com>
2024-08-08 21:52:01 +0800

    first commit

---
+++ app/_layout.tsx
@@ -0,0 +1,10 @@
+import '@/assets/css/global.css';
+import { Stack } from 'expo-router';
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+}
